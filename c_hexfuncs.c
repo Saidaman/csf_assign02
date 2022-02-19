8837f6126eee2f4cce148603d79d20a011eab634
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-19 17:00:13 -0500

    Added functions to c_hexfuncs.c

--- c_hexfuncs.c
+++ c_hexfuncs.c
@@ -4,3 +4,33 @@
 #include "hexfuncs.h"
 
 // TODO: add function implementations here
+
+unsigned hex_read(char data_buf[]) {
+    //TODO
+    assert(0);
+    return;
+}
+
+void hex_write_string(const char s[]) {
+    //TODO
+    assert(0);
+    return;
+}
+
+void hex_format_offset(unsigned offset, char sbuf[]) {
+    //TODO
+    assert(0);
+    return;
+}
+
+void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
+    //TODO
+    assert(0);
+    return;
+}
+
+char hex_to_printable(unsigned char byteval) {
+    //TODO
+    assert(0);
+    return;
+}
