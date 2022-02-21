1c9740ceef7439976cdbd6ff68b58c253564c8ae
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-20 22:07:28 -0500

    made changes to main

--- c_hexmain.c
+++ c_hexmain.c
@@ -3,5 +3,24 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(void) {
-  // TODO: implement the main function
+  char input[16] = "";
+  char address[8] = "";
+  unsigned chars_read = hex_read(input);
+
+  //for the first iteration
+  if (chars_read > 0) {
+    hex_format_offset(0, address);
+    printf("%s: ", address);
+  }
+  
+  //keep repeating while there is valid input
+  while (chars_read > 0) {
+    hex_format_offset(chars_read, address);
+    printf("%s: ", address);
+    
+
+
+    //update the new line of input
+    chars_read = hex_read(input);
+  }
 }
