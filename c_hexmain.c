77486f9fb61c562ebfdc13306b62a179ddc5f0ea
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-21 08:29:48 -0500

    Made additional changes to main

--- c_hexmain.c
+++ c_hexmain.c
@@ -7,12 +7,21 @@ int main(void) {
   char address[8] = "";
   unsigned chars_read = hex_read(input);
 
-  //for the first iteration
+  //for the first iteration, where the address must be 0
   if (chars_read > 0) {
     hex_format_offset(0, address);
+    //print position: column 1
     printf("%s: ", address);
+    //print ASCII values
+    for (int i = 0; i < chars_read; i++) {
+      printf("%u ", hex_to_printable(input[i]));
+    }
+    //need to print empty space for last line?
+    //print string representation of data
+    
   }
   
+  chars_read = hex_read(input);
   //keep repeating while there is valid input
   while (chars_read > 0) {
     hex_format_offset(chars_read, address);
