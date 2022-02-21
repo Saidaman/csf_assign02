5de06db6d5749fbb8e521c3da1e29a810378ba29
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-21 17:42:53 -0500

    edits to main

--- c_hexmain.c
+++ c_hexmain.c
@@ -3,39 +3,44 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(void) {
-  char input[17] = "";
+  char input_buf[16] = {0};
   char address[9] = "";
-  char hexVal[2] = "";
-  input[17] = '\0';
   address[9] = '\0';
-  unsigned chars_read = hex_read(input);
+  unsigned chars_read = 0;
 
-  //for the first iteration, where the address must be 0
-  if (chars_read > 0) {
-    hex_format_offset(0, address);
-    //print position: column 1
-    hex_write_string(address);
-    hex_write_string(" "); //empty space?
-    //print ASCII values
-    for (int i = 0; i < chars_read; i++) {
-      hex_format_byte_as_hex(input[i], hexVal);
-      hex_write_string(hexVal);
-      hex_write_string(" ");
+  while ((chars_read = hex_read(input_buf)) != 0) {
+    
+    if (chars_read < 16) {
+      break;
     }
-    //need to print empty space for last line?
-    //print string representation of data
-    
   }
+
+  // //for the first iteration, where the address must be 0
+  // if (chars_read > 0) {
+  //   hex_format_offset(0, address);
+  //   //print position: column 1
+  //   hex_write_string(address);
+  //   hex_write_string(" "); //empty space?
+  //   //print ASCII values
+  //   for (int i = 0; i < chars_read; i++) {
+  //     hex_format_byte_as_hex(input[i], hexVal);
+  //     hex_write_string(hexVal);
+  //     hex_write_string(" ");
+  //   }
+  //   //need to print empty space for last line?
+  //   //print string representation of data
+    
+  // }
   
-  chars_read = hex_read(input);
-  //keep repeating while there is valid input
-  while (chars_read > 0) {
-    hex_format_offset(chars_read, address);
-    //printf("%s: ", address); can't have printf
+  // chars_read = hex_read(input);
+  // //keep repeating while there is valid input
+  // while (chars_read > 0) {
+  //   hex_format_offset(chars_read, address);
+  //   //printf("%s: ", address); can't have printf
     
 
 
-    //update the new line of input
-    chars_read = hex_read(input);
-  }
+  //   //update the new line of input
+  //   chars_read = hex_read(input);
+  // }
 }
