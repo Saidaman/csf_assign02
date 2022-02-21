4968dd14fd3f902c9898c32287a6455a3d80070a
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-21 10:02:22 -0500

    More changes to main

--- c_hexmain.c
+++ c_hexmain.c
@@ -3,18 +3,23 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(void) {
-  char input[16] = "";
-  char address[8] = "";
+  char input[17] = "";
+  char address[9] = "";
+  input[17] = '\0';
+  address[9] = '\0';
   unsigned chars_read = hex_read(input);
 
   //for the first iteration, where the address must be 0
   if (chars_read > 0) {
     hex_format_offset(0, address);
     //print position: column 1
-    printf("%s: ", address);
+    hex_write_string(address);
+    hex_write_string(" "); //empty space?
     //print ASCII values
     for (int i = 0; i < chars_read; i++) {
-      printf("%u ", hex_to_printable(input[i]));
+      hex_format_byte_as_hex(input[i]);
+      hex_write_string(input[i]);
+      hex_write_string(" ");
     }
     //need to print empty space for last line?
     //print string representation of data
