e6f1be5ce5e12de6f89b5d24a083b381f85caf6d
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-02-22 07:46:03 -0500

    Completed main

--- c_hexmain.c
+++ c_hexmain.c
@@ -15,6 +15,7 @@ int main(void) {
   char hex_digits[3] = "";
   unsigned chars_read = 0;
   unsigned bytes_read = 0;
+  int need_exit = 0;
 
   while ((chars_read = hex_read(input_buf)) != 0) {
     //print column 1
@@ -23,38 +24,35 @@ int main(void) {
     hex_write_string(": "); //colon and space
 
     //print column 2
-    
-    if (chars_read < 16) {
-      break;
+    for (int i = 0; i < chars_read; i++) {
+      hex_format_byte_as_hex(input_buf[i], hex_digits);
+      hex_write_string(hex_digits);
+      hex_write_string(" ");
     }
+    if (chars_read < 16) {
+      //need to pad extra blank with spaces
+      for (int i = 16 - chars_read; i > 0; i--) {
+        //print 3 spaces
+        hex_write_string("   ");
+      }
+      need_exit = 1; //last line of input read in
+    }
+    //two spaces before string representation
+    hex_write_string("  ");
+
+    for (int i = 0; i < chars_read; i++) {
+      char char_to_print[2];
+      char_to_print[0] = hex_to_printable(input_buf[i]);
+      char_to_print[1]= '\0';
+      hex_write_string(&char_to_print);
+      }
+
+      //need to add a \n before the next x bytes read in
+      hex_write_string('\n');
+
+      //last line of input where the bytes read in are < 16
+      if (need_exit) {
+        break;
+      }
   }
-
-  // //for the first iteration, where the address must be 0
-  // if (chars_read > 0) {
-  //   hex_format_offset(0, address);
-  //   //print position: column 1
-  //   hex_write_string(address);
-  //   hex_write_string(" "); //empty space?
-  //   //print ASCII values
-  //   for (int i = 0; i < chars_read; i++) {
-  //     hex_format_byte_as_hex(input[i], hexVal);
-  //     hex_write_string(hexVal);
-  //     hex_write_string(" ");
-  //   }
-  //   //need to print empty space for last line?
-  //   //print string representation of data
-    
-  // }
-  
-  // chars_read = hex_read(input);
-  // //keep repeating while there is valid input
-  // while (chars_read > 0) {
-  //   hex_format_offset(chars_read, address);
-  //   //printf("%s: ", address); can't have printf
-    
-
-
-  //   //update the new line of input
-  //   chars_read = hex_read(input);
-  // }
 }
